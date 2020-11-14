dc10086dde0655503e8c9f637f959f6a5daa8c62
ItzFireable <fireableplayz@gmail.com>
2020-11-14 23:58:36 +0200

    Fixed data retrieval + package lock

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const smi = require('node-nvidia-smi');
 const DiscordRPC = require('discord-rich-presence')('777277529299746817');
 
-let rpcdata
+var rpcdata = {}
 let start = Date.now()
 
-setTimeout(function() {
-
+setInterval(function() {
     smi(function (err, data) {
         if (err) {
             console.warn(err);
         }
         rpcdata = data
     });
+},100)
 
+setInterval(function() {
     DiscordRPC.updatePresence({
         state: rpcdata["nvidia_smi_log"]["gpu"]["temperature"]["gpu_temp"].substring(0, rpcdata["nvidia_smi_log"]["gpu"]["temperature"]["gpu_temp"].length - 2) + "°C - " + rpcdata["nvidia_smi_log"]["gpu"]["utilization"]["gpu_util"].substring(0, rpcdata["nvidia_smi_log"]["gpu"]["utilization"]["gpu_util"].length - 2) + "% usage",
         details: rpcdata["nvidia_smi_log"]["gpu"]["product_name"],
@@ -21,5 +22,5 @@ setTimeout(function() {
         largeImageKey: 'nvidia-logo-1',
         largeImageText: rpcdata["nvidia_smi_log"]["gpu"]["fb_memory_usage"]["used"].substring(0, rpcdata["nvidia_smi_log"]["gpu"]["fb_memory_usage"]["used"].length - 4) + " / " + rpcdata["nvidia_smi_log"]["gpu"]["fb_memory_usage"]["total"] + " memory usage, " + rpcdata["nvidia_smi_log"]["gpu"]["power_readings"]["power_draw"].substring(0, rpcdata["nvidia_smi_log"]["gpu"]["power_readings"]["power_draw"].length - 2) + "W / " + rpcdata["nvidia_smi_log"]["gpu"]["power_readings"]["power_limit"].substring(0, rpcdata["nvidia_smi_log"]["gpu"]["power_readings"]["power_limit"].length - 2) + "W power usage",
         instance: true,
-      });
-},1000,false)
+    });
+},1000)
